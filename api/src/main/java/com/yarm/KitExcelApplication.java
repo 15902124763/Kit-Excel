c16ef61708ade212bab49da43b07b73e8ab30882
yarm <15902124763@163.com>
2019-09-04 21:09:35 +0800

    ok

---
+++ api/src/main/java/com/yarm/KitExcelApplication.java
@@ -0,0 +1,18 @@
+package com.yarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created with IDEA
+ * author:Yarm.Yang
+ * Date:2019/9/4
+ * Time:20:31
+ * Des:port default is 8080
+ */
+@SpringBootApplication
+public class KitExcelApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KitExcelApplication.class, args);
+    }
+}
